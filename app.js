bba72502a90ab6fc48594051984e63b1f87a22a6
NITESH SHARMA <72177146+nitesh2130@users.noreply.github.com>
2023-07-29 10:59:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const getcolor = () => {
+    const randomNumber = Math.floor(Math.random()* 16777215);
+    const randomcode = "#" +randomNumber.toString(16);
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById("color-code").innerText = randomcode;
+    navigator.clipboard.writeText(randomcode)
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+   // evevnt call
+    getcolor
+)
+
+//inistial call
+getcolor();
